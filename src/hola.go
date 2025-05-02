948791f5478d7540f250a6b6c31f49a25689d3c8
juan carlos zarate molina <jcarlosxyz@gmail.com>
2025-05-02 00:48:52 +0000

    Cmbios a la nube

---
+++ src/hola.go
@@ -0,0 +1,13 @@
+// ssh-keygen -t ed25519 -C "tu_correo@example.com
+package main
+import "fmt"
+
+func main(){
+	// Declarecion de contamtes 
+	const pi float64 = 3.14
+    base := 6.48
+	altura := 9.71
+	total := base * altura
+	fmt.Printf("El resultado de base * altura es--- %.4f \n",total)
+
+} 
